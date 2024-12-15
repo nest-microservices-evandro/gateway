f82afbad4a51576128c8d1e3a487e5aff4b8df2e
Evandro Calado <evandrocalado07@gmail.com>
2024-12-15 19:37:54 -0300

    🐳 chore: load environment variables

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './configs/envs';
 
 async function bootstrap() {
+  const logger = new Logger('NestApplication');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(envs.PORT);
+
+  logger.log(`Gateway is running on: http://localhost:${envs.PORT}`);
 }
 bootstrap();
