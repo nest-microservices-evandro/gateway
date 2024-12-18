57d2896dcb80c09a4daed0cb13d81a94a7c7eba1
Evandro Calado <evandrocalado07@gmail.com>
2024-12-18 19:01:40 -0300

    🐞 fix: variables .env

--- src/configs/envs.ts
+++ src/configs/envs.ts
@@ -3,16 +3,12 @@ import * as joi from 'joi';
 
 interface EnvVars {
   PORT: number;
-  ORDERS_MS_HOST: string;
-  ORDERS_MS_PORT: number;
   NATS_SERVERS: string[];
 }
 
 const envVarsSchema = joi
   .object<EnvVars>({
     PORT: joi.number().required(),
-    ORDERS_MS_HOST: joi.string().required(),
-    ORDERS_MS_PORT: joi.number().required(),
     NATS_SERVERS: joi.array().items(joi.string()).required(),
   })
   .unknown(true);
@@ -30,7 +26,5 @@ const envVars: EnvVars = value;
 
 export const envs = {
   PORT: envVars.PORT,
-  ORDERS_MS_HOST: envVars.ORDERS_MS_HOST,
-  ORDERS_MS_PORT: envVars.ORDERS_MS_PORT,
   NATS_SERVERS: envVars.NATS_SERVERS,
 };
