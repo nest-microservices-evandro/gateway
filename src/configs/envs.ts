ee2502913bd7c696dd6f285eb0dfee55a51f5e18
Evandro Calado <evandrocalado07@gmail.com>
2024-12-18 10:59:09 -0300

    🦄 refactor: change  connection with MS products to NATS

--- src/configs/envs.ts
+++ src/configs/envs.ts
@@ -3,23 +3,24 @@ import * as joi from 'joi';
 
 interface EnvVars {
   PORT: number;
-  PRODUCTS_MS_HOST: string;
-  PRODUCTS_MS_PORT: number;
   ORDERS_MS_HOST: string;
   ORDERS_MS_PORT: number;
+  NATS_SERVERS: string[];
 }
 
 const envVarsSchema = joi
   .object<EnvVars>({
     PORT: joi.number().required(),
-    PRODUCTS_MS_HOST: joi.string().required(),
-    PRODUCTS_MS_PORT: joi.number().required(),
     ORDERS_MS_HOST: joi.string().required(),
     ORDERS_MS_PORT: joi.number().required(),
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
   })
   .unknown(true);
 
-const { error, value } = envVarsSchema.validate(process.env);
+const { error, value } = envVarsSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+});
 
 if (error) {
   throw new Error(`Config validation error: ${error.message}`);
@@ -29,8 +30,7 @@ const envVars: EnvVars = value;
 
 export const envs = {
   PORT: envVars.PORT,
-  PRODUCTS_MS_HOST: envVars.PRODUCTS_MS_HOST,
-  PRODUCTS_MS_PORT: envVars.PRODUCTS_MS_PORT,
   ORDERS_MS_HOST: envVars.ORDERS_MS_HOST,
   ORDERS_MS_PORT: envVars.ORDERS_MS_PORT,
+  NATS_SERVERS: envVars.NATS_SERVERS,
 };
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -13,36 +13,30 @@ import {
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { catchError } from 'rxjs';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
-import { PRODUCT_SERVICE } from 'src/configs/services.constant';
+import { NATS_SERVICE } from 'src/configs/services.constant';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
 export class ProductsController {
   constructor(
-    @Inject(PRODUCT_SERVICE)
-    private readonly productsClient: ClientProxy,
+    @Inject(NATS_SERVICE)
+    private readonly client: ClientProxy,
   ) {}
 
   @Post()
   createProduct(@Body() createProductDto: CreateProductDto) {
-    return this.productsClient.send(
-      { cmd: 'create_product' },
-      createProductDto,
-    );
+    return this.client.send('create_product', createProductDto);
   }
 
   @Get()
   findAllProducts(@Query() paginationDto: PaginationDto) {
-    return this.productsClient.send(
-      { cmd: 'find_all_products' },
-      paginationDto,
-    );
+    return this.client.send('find_all_products', paginationDto);
   }
 
   @Get(':id')
   findOneProduct(@Param('id', ParseIntPipe) id: number) {
-    return this.productsClient.send({ cmd: 'find_one_product' }, { id }).pipe(
+    return this.client.send('find_one_product', { id }).pipe(
       catchError((error) => {
         throw new RpcException(error);
       }),
@@ -54,18 +48,16 @@ export class ProductsController {
     @Param('id', ParseIntPipe) id: number,
     @Body() updateProductDto: UpdateProductDto,
   ) {
-    return this.productsClient
-      .send({ cmd: 'update_product' }, { id, ...updateProductDto })
-      .pipe(
-        catchError((error) => {
-          throw new RpcException(error);
-        }),
-      );
+    return this.client.send('update_product', { id, ...updateProductDto }).pipe(
+      catchError((error) => {
+        throw new RpcException(error);
+      }),
+    );
   }
 
   @Delete(':id')
   removeProduct(@Param('id', ParseIntPipe) id: number) {
-    return this.productsClient.send({ cmd: 'remove_product' }, { id }).pipe(
+    return this.client.send('remove_product', { id }).pipe(
       catchError((error) => {
         throw new RpcException(error);
       }),
